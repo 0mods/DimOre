f44a8e3e8857d0e87a9b498ea24b69dd035c1617
AlgorithmLX <algorithmlx@0mods.team>
2026-05-22 01:03:02 +0700

    Rework gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,120 +1,11 @@
-import dev.kikugie.stonecutter.build.StonecutterBuildExtension
-import me.modmuss50.mpp.ReleaseType
+import gg.meza.stonecraft.mod
 
 val kotlinVersion: String by rootProject
-val modId = project.properties["mod_id"].toString()
-val modName = project.properties["mod_name"].toString()
-val modVersion = project.properties["mod_version"].toString()
-val license = project.properties["mod_license"].toString()
-
-val publishType = project.properties["publish.type"]?.toString()
-val isBeta = publishType != null && publishType == "beta"
-val isAlpha = publishType != null && publishType == "alpha"
 
 plugins {
+    id("gg.meza.stonecraft")
     kotlin("jvm")
     kotlin("plugin.serialization")
-
-    id("dev.isxander.modstitch.base")
-    id("me.modmuss50.mod-publish-plugin")
-}
-
-base.archivesName = "${modName}-${stonecutter.modPlatform}"
-version = "${stonecutter.minecraftVersion}-${project.properties["mod_version"].toString()}${when {
-    isBeta -> "-B"
-    isAlpha -> "-A"
-    else -> ""
-}}"
-
-val StonecutterBuildExtension.modPlatform get() = current.project.substringAfterLast('-')
-val StonecutterBuildExtension.minecraftVersion get() = project.properties["minecraft_version"]?.toString() ?: current.project.substringBeforeLast('-')
-
-val modLoaderVersions = mutableMapOf(
-    "fabric" to mutableMapOf("loader" to "0.19.2", // or can use {mc-ver}-loader
-        "1.19.2-api" to "0.77.0+1.19.2", "1.20.1-api" to "0.92.2+1.20.1", "1.21-api" to "0.102.0+1.21",
-        "1.21.1-api" to "0.116.4+1.21.1", "1.21.2-api" to "0.106.1+1.21.2", "1.21.3-api" to "0.114.1+1.21.3",
-        "1.21.4-api" to "0.119.4+1.21.4", "1.21.5-api" to "0.128.2+1.21.5", "1.21.6-api" to "0.128.2+1.21.6",
-        "1.21.7-api" to "0.129.0+1.21.7", "1.21.8-api" to "0.136.1+1.21.8", "1.21.9-api" to "0.134.1+1.21.9",
-        "1.21.10-api" to "0.138.4+1.21.10", "1.21.11-api" to "0.141.1+1.21.11", "26.1-api" to "0.145.1+26.1"
-    ),
-    // forge is dead lol. no more versions need
-    "forge" to mutableMapOf("1.19.2" to "1.19.2-43.4.2", "1.20.1" to "1.20.1-47.4.3", "1.21" to "1.21-51.0.8",),
-    "neoforge" to mutableMapOf("1.21" to "21.0.167", "1.21.1" to "21.1.197", "1.21.2" to "21.2.1-beta",
-        "1.21.3" to "21.3.95", "1.21.4" to "21.4.156", "1.21.5" to "21.5.96", "1.21.6" to "21.6.20-beta",
-        "1.21.7" to "21.7.25-beta", "1.21.8" to "21.8.52", "1.21.9" to "21.9.16-beta", "1.21.10" to "21.10.64",
-        "1.21.11" to "21.11.36-beta", "26.1" to "26.1.0.19-beta"
-    )
-)
-
-val parchmentVersion = mutableMapOf(
-    "1.19.2" to "2022.11.27", "1.20.1" to "2023.09.03", "1.21" to "2024.07.28", "1.21.1" to "2024.11.17",
-    "1.21.3" to "2024.12.07", "1.21.4" to "2025.03.23", "1.21.5" to "2025.06.15", "1.21.6" to "2025.06.29",
-    "1.21.7" to "2025.07.18", "1.21.8" to "2025.09.14", "1.21.9" to "2025.10.05", "1.21.10" to "2025.10.12"
-)
-
-java.toolchain.languageVersion = JavaLanguageVersion.of(when {
-    stonecutter.eval(stonecutter.minecraftVersion, ">1.21.11") -> 25
-    stonecutter.eval(stonecutter.minecraftVersion, "<=1.21.11") && stonecutter.eval(stonecutter.minecraftVersion, ">1.19.4") -> 21
-    else -> 17
-})
-
-modstitch {
-    minecraftVersion = stonecutter.minecraftVersion
-
-    loom {
-        val verArr = modLoaderVersions["fabric"]!!
-        fabricLoaderVersion = verArr["${stonecutter.minecraftVersion}-loader"]
-            ?: verArr["loader"]
-                    ?: error("Failed to find loader version for ${stonecutter.minecraftVersion} on fabric")
-
-        configureLoom {
-            mixin.useLegacyMixinAp = false
-        }
-    }
-
-    moddevgradle {
-        val verArr = modLoaderVersions["neoforge"]!!
-        neoForgeVersion = verArr[stonecutter.minecraftVersion] ?: error("Failed to find loader version for ${stonecutter.minecraftVersion} on neoforge")
-    }
-
-    parchmentVersion[stonecutter.minecraftVersion]?.let { ver ->
-        parchment {
-            version = ver
-        }
-    }
-
-    metadata {
-        modId = project.properties["mod_id"].toString()
-        modVersion = project.properties["mod_version"].toString()
-        modName = project.properties["mod_name"].toString()
-        modGroup = "com.algorithmlx"
-        modLicense = project.properties["mod_license"].toString()
-        modDescription = project.properties["mod_description"].toString()
-        modCredits = project.properties["mod_credits"].toString()
-        modAuthor = project.properties["mod_authors"].toString()
-
-        replacementProperties.put("mc_version", stonecutter.minecraftVersion)
-        if (modstitch.isLoom) {
-            val fabricLoader = modLoaderVersions["fabric"]!!["${stonecutter.minecraftVersion}-loader"]
-                ?: modLoaderVersions["fabric"]!!["loader"]
-                ?: error("Failed to find loader version for ${stonecutter.minecraftVersion} on fabric")
-            val flkVersion = project.properties["flk_version"].toString()
-
-            replacementProperties.put("fabric_loader", fabricLoader)
-            replacementProperties.put("java_version", project.java.toolchain.languageVersion.toString())
-            replacementProperties.put("fabric_api", modLoaderVersions["fabric"]!!["${stonecutter.minecraftVersion}-api"]!!)
-            replacementProperties.put("flk_version", "$flkVersion+kotlin.$kotlinVersion")
-        } else {
-            val klfVersion = project.properties["klf_version"].toString()
-            replacementProperties.put("neoforge_version", modLoaderVersions["neoforge"]!![stonecutter.minecraftVersion]!!)
-            replacementProperties.put("klf_version", klfVersion)
-        }
-    }
-
-    mixin {
-        addMixinsToModManifest = true
-        configs.register(modId)
-    }
 }
 
 repositories {
@@ -123,15 +14,9 @@ repositories {
 }
 
 dependencies {
-    if (modstitch.isLoom) {
+    if (mod.isFabric) {
         val flkVersion = project.properties["flk_version"].toString()
-        if (stonecutter.eval(stonecutter.minecraftVersion, "<=1.21.11")) {
-            "modImplementation"("net.fabricmc.fabric-api:fabric-api:${modLoaderVersions["fabric"]!!["${stonecutter.minecraftVersion}-api"]}")
-            "modImplementation"("net.fabricmc:fabric-language-kotlin:$flkVersion+kotlin.$kotlinVersion")
-        } else {
-            implementation("net.fabricmc.fabric-api:fabric-api:${modLoaderVersions["fabric"]!!["${stonecutter.minecraftVersion}-api"]}")
-            implementation("net.fabricmc:fabric-language-kotlin:$flkVersion+kotlin.$kotlinVersion")
-        }
+        modCompileClasspath("net.fabricmc:fabric-language-kotlin:$flkVersion+kotlin.$kotlinVersion")
     } else {
         val klfVersion = project.properties["klf_version"].toString()
         val klfLoaderVersion = project.properties["klf_loader_version"].toString()
@@ -139,67 +24,12 @@ dependencies {
     }
 }
 
-stonecutter {
-    constants {
-        put("fabric", modstitch.isLoom)
-        put("neoforge", modstitch.isModDevGradleRegular)
-        put("forge", modstitch.isModDevGradleLegacy)
-        put("forgelike", modstitch.isModDevGradle)
-    }
+tasks.processResources {
+    if (mod.isFabric) expand(
+        "fabric_loader" to mod.prop("loader_version"),
+        "java_version" to java.toolchain.languageVersion.toString(),
+        "fabric_api" to mod.prop("fabric_version"), "flk_version" to "${mod.prop("flk_version")}+kotlin.$kotlinVersion"
+    )
 }
 
-publishMods {
-    dryRun = false
 
-    if (project.file("nopub").exists()) {
-        println("No publish file exists (${project.name}/nopub). Skipping")
-        return@publishMods
-    }
-    file = modstitch.finalJarTask.flatMap { it.archiveFile }
-
-    changelog = rootProject.file("CHANGELOG.md").readText()
-
-    type = when {
-        isBeta -> ReleaseType.BETA
-        isAlpha -> ReleaseType.ALPHA
-        else -> ReleaseType.STABLE
-    }
-
-    displayName = "[${stonecutter.modPlatform}-${stonecutter.minecraftVersion}] ${modstitch.metadata.modName.get()} (v.${project.properties["mod_version"].toString()})"
-    modLoaders.add(stonecutter.modPlatform)
-
-    val modrinthProject: String? = rootProject.properties["modrinth_project"]?.toString()
-    val modrinthToken = System.getenv("MODRINTH_TOKEN")
-
-    val curseProject: String? = rootProject.properties["curseforge_project"]?.toString()
-    val curseToken = System.getenv("CURSE_TOKEN")
-
-    if (modrinthToken != null && modrinthProject != null) modrinth {
-        projectId = modrinthProject
-        accessToken = modrinthToken
-
-        if (modstitch.isLoom)
-            requires("fabric-api", "fabric-language-kotlin")
-        else requires("kotlin-lang-forge")
-
-        minecraftVersions.add(stonecutter.minecraftVersion)
-    }
-
-    if (curseToken != null && curseProject != null) curseforge {
-        projectId = curseProject
-        accessToken = curseToken
-
-        if (modstitch.isLoom)
-            requires("fabric-api", "fabric-language-kotlin")
-        else requires("kotlinlangforge")
-
-        if (stonecutter.minecraftVersion.contains("snapshot")) {
-            val modifiedVersion = buildString {
-                val oldVersion = stonecutter.minecraftVersion
-                val splitted = oldVersion.split("-")
-                append(splitted[0] + "-snapshot")
-            }
-            minecraftVersions.add(modifiedVersion)
-        } else minecraftVersions.add(stonecutter.minecraftVersion)
-    }
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,14 @@
+import java.util.Properties
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
-        maven("https://plugins.gradle.org/m2/")
-        maven("https://maven.parchmentmc.org")
+        maven("https://maven.kikugie.dev/releases")
         maven("https://maven.fabricmc.net/")
-        maven("https://repo.spongepowered.org/repository/maven-public/")
-        maven("https://maven.neoforged.net/releases")
         maven("https://maven.architectury.dev")
         maven("https://maven.minecraftforge.net")
-        maven("https://maven.kikugie.dev/snapshots")
-        maven("https://maven.isxander.dev/releases")
+        maven("https://maven.neoforged.net/releases/")
     }
 
     val kotlinVersion: String by settings
@@ -22,6 +20,7 @@ pluginManagement {
 
 plugins {
     id("dev.kikugie.stonecutter") version "0.9+"
+    id("gg.meza.stonecraft") version "1.10.+"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
@@ -30,12 +29,34 @@ stonecutter {
     centralScript = "build.gradle.kts"
 
     shared {
-        rootProject.projectDir.resolve("versions")
+        fun mc(version: String, vararg loaders: String) {
+            for (it in loaders) version("$version-$it", version)
+        }
+
+        rootProject.projectDir.resolve("versions/dependencies")
             .listFiles()
-            .filter { it.isDirectory }
-            .filter { !it.resolve(".build-ignore").exists() }
-            .forEach { version(it.name) }
+            .filter { !it.isDirectory }
+            .filter {
+                val props = Properties()
+                props.load(it.absoluteFile.inputStream())
+                !props.containsKey("build.ignore") && props["build.ignore"].toString() != "true"
+            }.forEach {
+                val props = Properties()
+                props.load(it.absoluteFile.inputStream())
+
+                val fileName = it.name
+
+                if (!props.containsKey("mod.platforms")) {
+                    throw NullPointerException("mod.platforms is required for compile! Problem file: $fileName")
+                }
+
+                val platforms = props.getProperty("mod.platforms").split(',')
+                val platformVersion = fileName.substringBeforeLast(".")
+
+                mc(platformVersion, *platforms.toTypedArray())
+            }
     }
+
     create(rootProject)
 }
 
