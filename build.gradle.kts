1766cfb06d339864d183873353b2de76c8254e85
algorithmlx <algorithmlx@0mods.team>
2026-05-22 13:48:46 +0700

    build: improvements

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 import gg.meza.stonecraft.mod
+import me.modmuss50.mpp.ReleaseType
 
+val publishType = if (mod.hasProp("build.release_type")) mod.prop("build.release_type") else null
+val isBeta = publishType != null && publishType == "beta"
+val isAlpha = publishType != null && publishType == "alpha"
 val kotlinVersion: String by rootProject
 
 plugins {
@@ -33,6 +37,77 @@ modSettings {
         "neoforge_version" to mod.prop("loader_version"), "klf_version" to mod.prop("klf_version")
     )
     variableReplacements.putAll(replaces)
+}
+
+publishMods {
+    dryRun = false
+
+    if (
+        mod.hasProp("build.no_publish")
+        && (
+            mod.prop("build.no_publish") == "true"
+                    || ((mod.prop("build.no_publish") == "neoforge") && mod.isNeoforge)
+                    || ((mod.prop("build.no_publish") == "fabric") && mod.isFabric)
+                    || ((mod.prop("build.no_publish") == "forge") && mod.isForge)
+                    || ((mod.prop("build.no_publish") == "fabriclike") && mod.isFabricLike)
+                    || ((mod.prop("build.no_publish") == "forgelike") && mod.isForgeLike)
+        )
+    ) {
+        println("Publishing disabled. Skipping...")
+        return@publishMods
+    }
+
+    val additionalMinecraftVersions = if (mod.hasProp("minecraft_version.additional"))
+        mod.prop("minecraft_version.additional").split(',').map { it.trim() }
+    else listOf()
+
+    changelog = rootProject.file("CHANGELOG.md").readText()
+
+    type = when {
+        isBeta -> ReleaseType.BETA
+        isAlpha -> ReleaseType.ALPHA
+        else -> ReleaseType.STABLE
+    }
+
+    displayName = "[${mod.loader}-${mod.minecraftVersion}] ${mod.name} (v.${mod.version})"
+    modLoaders.add(mod.loader)
+
+    val modrinthProject: String? = rootProject.properties["modrinth_project"]?.toString()
+    val modrinthToken = System.getenv("MODRINTH_TOKEN")
+
+    val curseProject: String? = rootProject.properties["curseforge_project"]?.toString()
+    val curseToken = System.getenv("CURSE_TOKEN")
+
+    if (modrinthToken != null && modrinthProject != null) modrinth {
+        projectId = modrinthProject
+        accessToken = modrinthToken
+
+        if (mod.isFabricLike)
+            requires("fabric-api", "fabric-language-kotlin")
+        else requires("kotlin-lang-forge")
+
+        minecraftVersions.add(mod.minecraftVersion)
+        minecraftVersions.addAll(additionalMinecraftVersions)
+    }
+
+    if (curseToken != null && curseProject != null) curseforge {
+        projectId = curseProject
+        accessToken = curseToken
+
+        if (mod.isFabricLike)
+            requires("fabric-api", "fabric-language-kotlin")
+        else requires("kotlinlangforge")
+
+        if (mod.minecraftVersion.contains("snapshot")) {
+            val modifiedVersion = buildString {
+                val oldVersion = mod.minecraftVersion
+                val splitted = oldVersion.split("-")
+                append(splitted[0] + "-snapshot")
+            }
+            minecraftVersions.add(modifiedVersion)
+        } else minecraftVersions.add(mod.minecraftVersion)
+        minecraftVersions.addAll(additionalMinecraftVersions)
+    }
 }
 
 fun DependencyHandlerScope.implementMod(dependencyNotation: Any) {
